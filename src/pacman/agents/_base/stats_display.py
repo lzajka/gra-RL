0c633de7909d37e634765e889867786b3ad4a251
lzajka <lukasz.mateusz.czajka@proton.me>
2025-08-29 07:33:17 +0200

    feat(player): Statystyki od teraz pokazują średni bonus/karę AI.
    Dodane dlatego, bo dłuższe odcinki mogą mieć większe kary.

--- src/pacman/agents/_base/stats_display.py
+++ src/pacman/agents/_base/stats_display.py
@@ -17,7 +17,7 @@ class StatsDisplay(AGameStatsDisplay):
         
         if state.is_game_over: 
             self.add_new_score(self.prev_score)
-            self.ai_bonuses.append(state.ai_bonus)
+            self.ai_bonuses.append(state.ai_bonus / state.move_num)
             self.prev_score = 0
         self.prev_score = score
 
